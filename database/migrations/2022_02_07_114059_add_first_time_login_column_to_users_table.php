cb69bffc09b6383ecea88d4260e653c3874546a7
Muhammad Hanis Irfan <hanisirfan.work@gmail.com>
2022-02-07 12:52:35 +0800

    Added a reminder message for first time login For updating their profile and for updating their password.

---
+++ database/migrations/2022_02_07_114059_add_first_time_login_column_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFirstTimeLoginColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_time_login', 5)->after('remember_token')->default('yes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('first_time_login');
+        });
+    }
+}
