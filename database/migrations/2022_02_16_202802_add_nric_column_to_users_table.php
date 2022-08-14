73d6132dc840cc2fcb6494d0cbc73b5cfe2adf03
Muhammad Hanis Irfan <hanisirfan.work@gmail.com>
2022-08-14 19:53:05 +0800

    Fixed identification_number issue when installing the system

--- database/migrations/2022_02_16_202802_add_nric_column_to_users_table.php
+++ database/migrations/2022_02_16_202802_add_nric_column_to_users_table.php
@@ -24,8 +24,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('identification_number');
-        });
+
     }
 };
